6ffbe3e5549b9fa61aeb9b13f4be7a1708d9e2c6
Tim Chapin <chapin.timothys@gmail.com>
2023-02-14 11:14:25 +0900

    Removed list controller and used link_to instead

--- app/javascript/controllers/list_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  show(e) {
-    let base_url = e.srcElement.baseURI
-    let list_id = e.currentTarget.getAttribute("data-id")
-    window.location.href =`${base_url}/${list_id}`
-  }
-}
