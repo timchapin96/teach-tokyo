deff8cf2f08111e96181d82ddbc04ab16e90b9d3
Tim Chapin <chapin.timothys@gmail.com>
2023-02-17 16:15:39 +0900

    Local version of email auth working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,7 +38,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: ENV.fetch('MAIL_HOST') }
+  config.action_mailer.default_url_options = { host: "localhost:3000" }
   config.action_mailer.smtp_settings = {
     user_name:      ENV.fetch('SENDMAIL_USERNAME'),
     password:       ENV.fetch('SENDMAIL_PASSWORD'),
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_062646) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_17_035530) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_062646) do
     t.datetime "updated_at", null: false
     t.string "nickname"
     t.string "avatar"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
