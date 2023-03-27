721e70ee3216199ba5e18468cac39ced48d0a80d
Tim Chapin <chapin.timothys@gmail.com>
2023-03-27 15:07:27 +0900

    Added moving clouds

--- app/javascript/controllers/load_controller.js
+++ app/javascript/controllers/load_controller.js
@@ -1,9 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
   load() {
-    console.log("Hi");
     let gamesList = document.querySelector(".load-games")
     gamesList.style.display = "block";
   }
