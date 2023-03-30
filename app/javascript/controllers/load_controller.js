f19803747329f54ec976754d69dfa85a720a0775
Tim Chapin <chapin.timothys@gmail.com>
2023-03-30 11:55:58 +0900

    Changed number labels on hover from white to black

--- app/javascript/controllers/load_controller.js
+++ app/javascript/controllers/load_controller.js
@@ -4,5 +4,8 @@ export default class extends Controller {
   load() {
     let gamesList = document.querySelector(".load-games")
     gamesList.style.display = "block";
+    window.onbeforeunload = function() {
+      gamesList.style.display = "none";
+    }
   }
 }
