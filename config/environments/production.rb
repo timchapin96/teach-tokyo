775604507938aebee0b1658602ece78cb1e8a000
Tim Chapin <chapin.timothys@gmail.com>
2023-05-17 11:12:50 +0900

    added www to mailer config url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # require "syslog/logger"
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new "app-name")
-  config.action_mailer.default_url_options = { host: 'teachtokyo.com' }
+  config.action_mailer.default_url_options = { host: 'www.teachtokyo.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
