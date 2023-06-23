3186192b28dc75f290443c5858f7cce04eab5261
Tim Chapin <chapin.timothys@gmail.com>
2023-06-23 12:02:15 +0900

    Added user authentication to all but the home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  skip_before_action :authenticate_user!, only: :home
   def current_controller?(names)
     names.include?(current_controller)
   end
