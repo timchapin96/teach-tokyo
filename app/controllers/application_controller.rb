fabacecef7633c405f941aba584608e78118ab5c
Tim Chapin <chapin.timothys@gmail.com>
2023-06-25 17:32:13 +0900

    Added Japanese options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   skip_before_action :authenticate_user!, only: :home
+  around_action :switch_locale
+
   def current_controller?(names)
     names.include?(current_controller)
+  end
+
+  def parse_yaml(file)
+    YAML.load(File.open(file))
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  def switch_locale(&)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &)
   end
 end
