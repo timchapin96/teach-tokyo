f2046cffe420dbb27b6bebf8780a243a72bbee19
Tim Chapin <chapin.timothys@gmail.com>
2023-07-28 15:10:24 +0900

    Added team suffle method to randomize turn order

--- app/javascript/controllers/round_controller.js
+++ app/javascript/controllers/round_controller.js
@@ -1,18 +1,52 @@
 import { Controller } from "@hotwired/stimulus"
+const roundLength = 0;
+const turnOrder = [];
 
 export default class extends Controller {
   static targets = ["roundPopup", "show"];
+  static values = {
+    teams: Array,
+    game: Object
+  }
 
-  connect () {
+  connect() {
+    // const shuffled = this.shuffle();
+    // console.log(shuffled);
+    roundLength = this.teamsValue.length;
+  }
+
+  round() {
 
   }
-  //Randomize team turn order
-  //set round length to number of teams
+
   //Team can pick one territory, then it moves on to next teams turn
+  turn()
   //When round is over add 1 to round start from top of the order
   //When a new round starts display that round.
   newRound() {
     this.roundPopupTarget.showModal();
   }
 
+  //Randomize team turn order
+  //Fisher-Yates Shuffle
+  shuffle() {
+    let teams = this.teamsValue.map((team) => {
+      return team.color
+    });
+    let currentIndex = this.teamsValue.length, randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [teams[currentIndex], teams[randomIndex]] = [
+        teams[randomIndex], teams[currentIndex]];
+    }
+
+    return teams;
+  }
 }
