929f7f05eaec516da3ba14a202effae091efca33
Tim Chapin <chapin.timothys@gmail.com>
2023-10-03 14:45:40 +0900

    Readded migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_17_074715) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_02_071950) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,11 +24,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_074715) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id", null: false
-    t.string "selectedTeams", default: [], array: true
-    t.boolean "newGame", default: true
-    t.json "teamScores", default: {}
-    t.index ["user_id"], name: "index_games_on_user_id"
   end
 
   create_table "messages", force: :cascade do |t|
@@ -68,7 +63,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_17_074715) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "games", "users"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
 end
